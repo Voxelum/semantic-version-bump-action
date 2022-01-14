48813a3697b6a59ffed15e585fd7bcae4234c674
CI010 <cijhn@hotmail.com>
2022-01-15 00:59:21 +0800

    fix: add deps changelog

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,7 @@ function renderChangelog(update: PackageUpdate, dedicated: boolean): string {
     }
     if (reasons.deps.length !== 0) {
         body += `${padding} Dependencies Updates\n\n`
-        reasons.deps.map(d => `Dependency ${d.name} bump **${d.releaseType}**`)
+        reasons.deps.map(d => `Dependency ${d.name} bump **${d.releaseType}**`).forEach(l => body += l);
     }
 
     return body
